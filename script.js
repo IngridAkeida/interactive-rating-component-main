36e3e223dfe5f0bdf8a8f7dc26dabd60fa3853b9
IngridAkeida <ingridsanchesp@gmail.com>
2023-11-04 21:24:37 +0100

    add - jsInfo

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 function submit() {
 
-  var select= '';
+  let select = document.getElementById("info");
+  let five = document.getElementById("five");
+  let four = document.getElementById("four");
+  let three = document.getElementById("three");
+  let two = document.getElementById("two");
+  let one = document.getElementById("one");
+
+  select = '';
+
+
+
+
+
+
+
+  
+
+  
 
   if(!select){
     alert(`Please chose a button`)
   }else{
-    
+    if()
+    daysRes.innerHTML = `${dayAge}`;
   }
   
 }
