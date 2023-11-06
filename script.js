709051b50685e44bd6464542b08970be36907f77
IngridAkeida <ingridsanchesp@gmail.com>
2023-11-06 15:42:15 +0100

    update commit

--- script.js
+++ script.js
@@ -19,58 +19,12 @@ function submit() {
         }
     }
     // show the output:
-    if(selected = true){
-
+    if(!selected){
+      alert(`Please chose a button`);
+    } else {
       page1.style.display = "none";
       page2.style.display = "block";
-      select.innerHTML = `You selected ${selected} out of 5`;
-
-    } else {
-      alert(`Please chose a button`);
+      select.innerHTML += `You selected ${selected} out of 5`;
     }
-    /*select.innerText = selected ? 
-    
-    `You selected ${selected}` : 
-    `You haven't selected any size`;*/
-});
-
-  /*
-  let five = document.getElementById("five").value;
-  let four = document.getElementById("four").value;
-  let three = document.getElementById("three").value;
-  let two = document.getElementById("two").value;
-  let one = document.getElementById("one").value;
-
-  let page1 = document.getElementById("page1");
-  let page2 = document.getElementById("page2");
-
-  let valueRate = '';
-
-
-
-
-  if(button == ''){
-    alert(`Please chose a button`);
-  }else{
-
-    if(five){
-      valueRate = five;
-    }
-    if(four){
-      valueRate = four;
-    }
-    if(three){
-      valueRate = three;
-    }
-    if(two){
-      valueRate = two;
-    }
-    if(one){
-      valueRate = one;
-    }
-
-    select.innerHTML = `You selected ${valueRate} out of 5`;
-    
-  }*/
-  
+  }); 
 }
