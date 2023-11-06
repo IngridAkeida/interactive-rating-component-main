eea648ee24f889c135231212d6bdfbcbadb7b5af
IngridAkeida <ingridsanchesp@gmail.com>
2023-11-06 15:18:12 +0100

    update commit

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 function submit() {
   let select = document.getElementById("info").value;
 
+  let button = document.querySelector('button');
+
   const numberButtons = document.querySelectorAll('input[name="rateSelected"]');
 
-  
+  button.addEventListener("click", () => {
 
+    let selected;
+
+    for (const numberButton of numberButtons) {
+        if (numberButton.checked) {
+          selected = numberButton.value;
+          break;
+        }
+    }
+    // show the output:
+    if(selected){
+
+      page1.style.display = "none";
+      page2.style.display = "block";
+      select.innerHTML = `You selected ${selected} out of 5`;
+
+    } else {
+      alert(`Please chose a button`);
+
+    }
+    /*select.innerText = selected ? 
+    
+    `You selected ${selected}` : 
+    `You haven't selected any size`;*/
+});
+
+  /*
   let five = document.getElementById("five").value;
   let four = document.getElementById("four").value;
   let three = document.getElementById("three").value;
   let two = document.getElementById("two").value;
   let one = document.getElementById("one").value;
-
-  let button = document.getElementById("button").value;
 
   let page1 = document.getElementById("page1");
   let page2 = document.getElementById("page2");
@@ -24,9 +50,6 @@ function submit() {
   if(button == ''){
     alert(`Please chose a button`);
   }else{
-
-    page1.style.display = "none";
-    page2.style.display = "block";
 
     if(five){
       valueRate = five;
@@ -46,6 +69,6 @@ function submit() {
 
     select.innerHTML = `You selected ${valueRate} out of 5`;
     
-  }
+  }*/
   
 }
