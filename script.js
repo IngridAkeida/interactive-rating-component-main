85aafa51cecf61deffd256e4a62917b419759553
IngridAkeida <ingridsanchesp@gmail.com>
2023-11-06 12:43:59 +0100

    update commit

--- script.js
+++ script.js
@@ -9,16 +9,12 @@ function submit() {
   let page1 = document.getElementById("page1");
   let page2 = document.getElementById("page2");
 
-  let number = {
-
-  }
-
   select = '';
 
   if(!select){
     alert(`Please chose a button`);
   }else{
-    
+
     page1.style.display = "none";
     page2.style.display = "block";
 
